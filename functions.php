332828a744d6041dac46d128dbe4881b1c80a55b
tereshok <43421381+tereshok@users.noreply.github.com>
2021-07-23 17:21:50 +0300

    update | update header, footer and front-page files

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once (get_template_directory() .'/inc/wp/enqueue_scripts.php');
+require_once (get_template_directory() .'/inc/wp/after_setup_theme.php');
+
+require_once (get_template_directory() .'/inc/acf/acf_map_init.php');
+require_once (get_template_directory() .'/inc/acf/acf_option_init.php');
