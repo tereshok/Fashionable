d8afe7bae245022fa5af6f1b462ca81f9d50c0df
tereshok <43421381+tereshok@users.noreply.github.com>
2021-07-23 16:57:30 +0300

    update | update helpers file

---
+++ inc/wp/enqueue_scripts.php
@@ -0,0 +1,13 @@
+<?php
+
+add_action('wp_enqueue_scripts', function(){
+  wp_enqueue_style('bootstrap', get_template_directory_uri() . '/assets/styles/bootstrap.min.css');
+  wp_enqueue_style('jquery-fancybox', get_template_directory_uri() . '/assets/styles/jquery.fancybox.min.css');
+  wp_enqueue_style('main-css', get_template_directory_uri() . '/assets/styles/main.css');
+  
+  wp_enqueue_script('jquery');
+  wp_enqueue_script('bootstrap-bundle-min', get_template_directory_uri() . '/scripts/bootstrap.bundle.min.js');
+  wp_enqueue_script('jquery-fancybox', get_template_directory_uri() . '/scripts/jquery.fancybox.min.js');
+  wp_enqueue_script('main-js', get_template_directory_uri() . '/scripts/main.js');
+
+});
