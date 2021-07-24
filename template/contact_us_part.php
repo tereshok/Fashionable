5628944876f56df93974607a3949ce4ec5549989
tereshok <43421381+tereshok@users.noreply.github.com>
2021-07-24 20:59:18 +0300

    update | update contact-us styles

--- template/contact_us_part.php
+++ template/contact_us_part.php
@@ -1,13 +1,13 @@
-<section class="container-big" style="background-image: url('<?php esc_url(the_field('contact_us_background')) ?>');">
-  <div class="container-small">
+<section class="container-big contact-us" style="background-image: url('<?php esc_url(the_field('contact_us_background')) ?>');">
+  <div class="container-small contact-us-container">
     <?php if(get_field('contact_us_first_line')) : ?>
-      <h5><?php esc_html(the_field('contact_us_first_line')) ?></h5>
+      <h5 class="contact-us-first-title"><?php esc_html(the_field('contact_us_first_line')) ?></h5>
     <?php endif; ?>
     <?php if(get_field('contact_us_second_line')) : ?>
-      <h5><?php esc_html(the_field('contact_us_second_line')) ?></h5>
+      <h5 class="contact-us-second-title"><?php esc_html(the_field('contact_us_second_line')) ?></h5>
     <?php endif; ?>
     <?php if(get_field('contact_us_link_title')) : ?>
-      <a href="#"><?php esc_html(the_field('contact_us_link_title')) ?></a>
+      <a href="#" class="contact-us-link"><?php esc_html(the_field('contact_us_link_title')) ?></a>
     <?php endif; ?>
   </div>
 </section>
