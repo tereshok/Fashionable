9f7c8c6f14f6804d355cd2b73f59bd1969adc869
tereshok <43421381+tereshok@users.noreply.github.com>
2021-07-24 15:55:46 +0300

    update | update footer file

--- footer.php
+++ footer.php
@@ -1,3 +1,34 @@
+<div class="container-big">
+  <div>
+    <?php if(get_field('theme_logo', 'option')) : ?>
+      <a href="<?php echo esc_url(home_url()) ?>">
+        <img src="<?php esc_url(the_field('theme_logo', 'option')) ?>" alt="<?php bloginfo('name') ?>">
+      </a>
+    <?php endif; ?>
+  </div>
+  <div>
+    <p>
+      <?php if(get_field('copyright_before_text', 'option')) : ?>
+        <?php esc_html(the_field('copyright_before_text', 'option'))?>
+      <?php endif; ?>
+    </p>
+    <p>
+      <?php echo date("Y") ?>
+    </p>
+    <p>
+      <?php if(get_field('copyright_after_text', 'option')) : ?>
+        <?php esc_html(the_field('copyright_after_text', 'option'))?>
+      <?php endif; ?>  
+    </p>
+    <p>      
+      <?php if(get_field('copyright_link', 'option')) : ?>
+        <a href="<?php esc_url(the_field('copyright_link', 'option')) ?>">
+          <?php esc_html(the_field('copyright_link_title', 'option')) ?>
+        </a>
+      <?php endif; ?>    
+    </p>
+  </div>
+</div>
 <?php wp_footer();?>
   </body>
 </html>
