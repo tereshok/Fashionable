fdcddd403969a926719d23fc31a5e8e747dd741f
tereshok <43421381+tereshok@users.noreply.github.com>
2021-07-24 22:04:22 +0300

    update | update price-list styles

--- template/price_list_part.php
+++ template/price_list_part.php
@@ -1,57 +1,57 @@
-<section class="container-big" style="background-image: url('<?php esc_url(the_field('price_list_section_background')) ?>');">
+<section class="container-big price-list" style="background-image: url('<?php esc_url(the_field('price_list_section_background')) ?>');">
   <div class="container-small">
     <div>
       <?php if(get_field('price_list_title')) : ?>
-        <h3><?php esc_html(the_field('price_list_title')) ?></h3>
+        <h3 class="price-list-title"><?php esc_html(the_field('price_list_title')) ?></h3>
       <?php endif; ?>
       <?php if(get_field('price_list_subtitle')) : ?>
-        <h5><?php esc_html(the_field('price_list_subtitle')) ?></h5>
+        <h5 class="price-list-subtitle"><?php esc_html(the_field('price_list_subtitle')) ?></h5>
       <?php endif; ?>
       <?php if(get_field('price_list_subtext')) : ?>
-        <p><?php esc_html(the_field('price_list_subtext')) ?></p>
+        <div class="price-list-subtext"><?php esc_html(the_field('price_list_subtext')) ?></div>
       <?php endif; ?>
     </div>
     <div>
       <?php if(get_field('price_list_table_title')) : ?>
-        <h3><?php esc_html(the_field('price_list_table_title')) ?></h3>
+        <h3 class="price-list-table-title"><?php esc_html(the_field('price_list_table_title')) ?></h3>
       <?php endif; ?>
       <div>
-        <div class="row">
+        <div class="row price-list-table-head">
           <?php if( have_rows('price_list_table_head') ): ?>
             <?php while( have_rows('price_list_table_head') ) : the_row(); ?>
-              <div class="col-2">
-                <p><?php esc_html(the_sub_field('table_head_text')) ?></p>
-              </div>
+              <span class="col-2 table-head-text">
+                <?php esc_html(the_sub_field('table_head_text')) ?>
+              </span>
             <?php endwhile; ?>
           <?php endif; ?>
         </div>
-        <div class="row">
+        <div class="row price-list-table">
           <?php if( have_rows('price_list_table') ): ?>
             <?php while( have_rows('price_list_table') ) : the_row(); ?>
-              <div class="col-2">
-                <p><?php esc_html(the_sub_field('length')) ?></p>
+              <div class="col-2 length">
+                <?php esc_html(the_sub_field('length')) ?>
               </div>
               <div class="col-2">
-                <p><?php esc_html(the_sub_field('first_price')) ?></p>
+                <?php esc_html(the_sub_field('first_price')) ?>
               </div>
               <div class="col-2">
-                <p><?php esc_html(the_sub_field('second_price')) ?></p>
+                <?php esc_html(the_sub_field('second_price')) ?>
               </div>
               <div class="col-2">
-                <p><?php esc_html(the_sub_field('third_price')) ?></p>
+                <?php esc_html(the_sub_field('third_price')) ?>
               </div>
               <div class="col-2">
-                <p><?php esc_html(the_sub_field('fourth_price')) ?></p>
+                <?php esc_html(the_sub_field('fourth_price')) ?>
               </div>
               <div class="col-2">
-                <p><?php esc_html(the_sub_field('fifth_price')) ?></p>
+                <?php esc_html(the_sub_field('fifth_price')) ?>
               </div>
             <?php endwhile; ?>
           <?php endif; ?>
         </div>
       </div>
     </div>
-    <div>
+    <div class="price-list-after-table-text">
       <?php if(get_field('price_list_after_table_text')) : ?>
         <?php esc_html(the_field('price_list_after_table_text')) ?>
       <?php endif; ?>
