a6d9254be8ca65fbb208783583d89995733861b9
tereshok <43421381+tereshok@users.noreply.github.com>
2021-07-24 15:13:33 +0300

    update | update contact-author-part structure

---
+++ template/contact_author_part.php
@@ -0,0 +1,42 @@
+<section class="container-big" style="background-image: url('<?php esc_url(the_field('contact_section_background')) ?>');">
+  <div class="container-small">
+    <div>
+      <?php if(get_field('contact_author_title')) : ?>
+        <h3><?php esc_html(the_field('contact_author_title'))?></h3>
+      <?php endif; ?>
+      <?php if(get_field('contact_author_subtitle')) : ?>
+        <h3><?php esc_html(the_field('contact_author_subtitle'))?></h3>
+      <?php endif; ?>
+    </div>
+    <div class="row">
+        <div class="col-6">
+          <?php if(get_field('contact_form_shortcode')) : ?>
+            <?php esc_html(the_field('contact_form_shortcode'))?>
+          <?php endif; ?>
+        </div>
+        <div class="col-6">
+          <?php
+        $location = get_field('contact_map');
+        if( $location ): ?>
+          <div class="acf-map" data-zoom="16">
+              <div class="marker" data-lat="<?php echo esc_attr($location['lat']); ?>" data-lng="<?php echo esc_attr($location['lng']); ?>"></div>
+          </div>
+        <?php endif; ?>
+        </div>
+        <div class="col-6">
+          <?php if(get_field('contact_phone_number')) : ?>
+            <p><?php esc_html(the_field('contact_phone_number'))?></p>
+          <?php endif; ?>
+          <?php if(get_field('contact_email')) : ?>
+            <p><?php esc_html(the_field('contact_email'))?></p>
+          <?php endif; ?>
+        </div>
+        <div class="col-6">
+          <?php if(get_field('contact_addres')) : ?>
+            <p><?php esc_html(the_field('contact_addres'))?></p>
+          <?php endif; ?>
+        </div>
+    </div>
+   
+  </div>
+</section>
